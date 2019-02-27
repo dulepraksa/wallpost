d64d75a64367b4801d16141222d816986dfccb83
Aleksandar Zidai <aleksandar.zidai@gmail.com>
2019-02-27 10:31:45 +0100

    [33091] Changed Welcome page - added vuejs

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,6 +9,12 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+/*import bootstrap from './bootstrap'
+import Vue from 'vue'
+import WallpostList from './components/WallpostList.vue'*/
+
+
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -21,6 +27,9 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('wallpost-list', require('./components/WallpostList.vue').default)
+console.log('aaaaa')
+/*Vue.component('wallpost-list', WallpostList)*/
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
---
+++ resources/js/components/WallpostList.vue
@@ -0,0 +1,43 @@
+<template>
+    <form id="wrapper" method="post">
+        <div class="container"></p>
+            <div class="item">User</div>
+            <div class="item">Date & Time</div>
+            <div class="item">Location</div>
+
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    ...
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </div>
+        </div>         
+
+        <div class="container">
+            <a class="item">label1</a>
+            <a class="item">label2</a>
+            <a class="item">label3</a>
+            <a class="item">label4</a>
+            <a class="item">label5</a>
+        </div>
+
+        <div><textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Post text area"> </textarea></div><br>
+        <div><textarea class="form-control-file" id="exampleFormControlTextarea1" rows="3" placeholder="Post attachment area"> </textarea></div>
+        <div>No. of views   <input class="float-right" type="submit" value="Submit"></div><br>
+    </form>
+</template>
+
+<script>
+    export default {
+        ready: function () {
+
+        },
+        mounted() {
+            console.log('Component mounted3.')
+        }
+    }
+</script>
