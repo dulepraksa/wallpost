03d58e09b69eda630c351bc45950632d1754b513
dulemagija <dule.magija97@gmail.com>
2019-02-27 15:01:10 +0100

    [33091]-List of all wallposts with api provided data

--- resources/js/components/WallpostList.vue
+++ resources/js/components/WallpostList.vue
@@ -1,43 +1,33 @@
 <template>
-    <form id="wrapper" method="post">
-        <div class="container"></p>
-            <div class="item">User</div>
-            <div class="item">Date & Time</div>
-            <div class="item">Location</div>
-
-            <div class="dropdown">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    ...
-                </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-            </div>
-        </div>         
-
-        <div class="container">
-            <a class="item">label1</a>
-            <a class="item">label2</a>
-            <a class="item">label3</a>
-            <a class="item">label4</a>
-            <a class="item">label5</a>
-        </div>
-
-        <div><textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Post text area"> </textarea></div><br>
-        <div><textarea class="form-control-file" id="exampleFormControlTextarea1" rows="3" placeholder="Post attachment area"> </textarea></div>
-        <div>No. of views   <input class="float-right" type="submit" value="Submit"></div><br>
-    </form>
+<div>
+     <div class="wallpost" v-for="wallpost in wallposts">
+         <p>
+         <strong>{{wallpost.title}}</strong>
+            <br>
+            {{wallpost.body}}
+        </p>
+    </div>
+</div>
 </template>
 
 <script>
     export default {
-        ready: function () {
-
+        data: function () {
+            return {
+                wallposts: []
+            }
         },
-        mounted() {
-            console.log('Component mounted3.')
+        mounted: function () {           
+            this.getAllPosts()
+        },
+        methods: {
+            getAllPosts : function () {                
+                axios.get('/api/wallpost')
+                    .then(response => {
+                        console.log(response)
+                        this.wallposts = response.data
+                    })
+            }
         }
     }
 </script>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
 <div class="container">
-    This is index.blade.php
     <wallpost-list></wallpost-list>
 </div>
 @endsection
